ad4af9e8cbd52172f6c7b61775407ac67f4145d8
Reyyan ARIK <reyyan.arik@nesine.com>
2020-03-22 17:03:55 +0300

    created project folders

---
+++ myserver.js
@@ -0,0 +1,64 @@
+var http = require("http")
+var express = require("express")
+var url = require("url")
+var fs = require("fs")
+//TODO: fs ile regex yazılmıyor, globu araştır.
+const glob = require('glob');
+
+
+var app = express();
+
+app.get('/', function(req, res) {
+    console.log(req.url);
+    res.sendFile( __dirname + "/index.html");
+})
+
+var directoryList = []
+var directoryInnerList = []
+
+const testFolder =  './allure-results/'
+fs.readdirSync(testFolder).forEach(file => {
+    if (file != '.DS_Store') {
+        directoryList.push(file);
+            app.get('/'+file, function(req, res){ 
+                console.log(req.url);
+             res.sendFile( __dirname + '/allure-results/'+file+'/index.html');    
+            });
+    }
+});
+
+console.log(directoryList);
+
+
+app.get('/folderList', function(req, res){
+    console.log(req.url);
+    res.send(directoryList);
+//    res.send(directoryList);  
+});
+
+/*
+app.get('/a', function(req, res){ 
+    console.log(req.url);
+    res.sendFile( __dirname + '/allure-results/a/index.html');    
+        
+});
+
+app.get('/b', function(req, res){ 
+    console.log(req.url);
+    res.sendFile( __dirname + '/allure-results/b/index.html');    
+        
+});
+*/
+
+for (var i = 0; i < directoryList.length; i++) {
+    app.get('/'+directoryList[i], function(req, res){ 
+    console.log(req.url);
+    res.sendFile( __dirname + '/allure-results/'+directoryList[i]+'/index.html');    
+        
+    });
+}
+
+app.listen(80, function(){
+    console.log('Server running at http://127.0.0.1:80/');
+});
+
