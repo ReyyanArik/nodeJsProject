fcfa9bc0c6c97c012bac6cc5b3c4946dcf4a4d02
Reyyan ARIK <reyyan.arik@nesine.com>
2020-04-12 02:01:17 +0300

    refactor

--- myserver.js
+++ myserver.js
@@ -6,80 +6,24 @@ var fs = require("fs")
 var app = express();
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/allure-results'));
+app.use(express.static(__dirname + '/allure-results/bddweb'));
+app.use(express.static(__dirname + '/allure-results/bddandroid'));
+app.use(express.static(__dirname + '/allure-results/bddios'));
+app.use(express.static(__dirname + '/allure-results/karate'));
 
 var home = __dirname + "/boxes.html";
+var tosProjects = ["bddweb","bddandroid","bddios","karate"];
+var fileNameRegex = new RegExp(/\d{14}/);
 
 app.get('/', function(req, res) {
     console.log(req.url);
     res.sendFile(home);
 })
 
-var fileNameRegex = new RegExp(/\d{14}/);
-
-app.get('/bddweb', function(req, res){
-    var webDirectoryList = []
-    console.log(req.url);
-    const bddWebFolder =  './allure-results/bddweb'
-    fs.readdirSync(bddWebFolder).forEach(file => {
-        if (fileNameRegex.test(file)) {
-            webDirectoryList.push(file);
-                app.get('/'+file, function(req, res){ 
-                 res.sendFile( __dirname + '/allure-results/bddweb/'+file+'/index.html');    
-                });
-        }
+tosProjects.forEach(tosProject =>{
+    app.get('/'+tosProject, function(req, res){
+        getTosReportByFolderName('bddweb',req,res);
     });
-    console.log(webDirectoryList);
-    res.send(webDirectoryList);
-});
-
-
-app.get('/bddandroid', function(req, res){
-    var androidDirectoryList = []
-    console.log(req.url);
-    const bddAndroidFolder =  './allure-results/bddandroid'
-    fs.readdirSync(bddAndroidFolder).forEach(file => {
-        if (fileNameRegex.test(file)) {
-            androidDirectoryList.push(file);
-                app.get('/'+file, function(req, res){ 
-                 res.sendFile( __dirname + '/allure-results/bddandroid/'+file+'/index.html');    
-                });
-        }
-    });
-    console.log(androidDirectoryList);
-    res.send(androidDirectoryList);
-});
-
-
-app.get('/bddios', function(req, res){
-    var iosDirectoryList = []
-    console.log(req.url);
-    const bddIosFolder =  './allure-results/bddios'
-    fs.readdirSync(bddIosFolder).forEach(file => {
-        if (fileNameRegex.test(file)) {
-            iosDirectoryList.push(file);
-                app.get('/'+file, function(req, res){ 
-                 res.sendFile( __dirname + '/allure-results/bddios/'+file+'/index.html');    
-                });
-        }
-    });
-    console.log(iosDirectoryList);
-    res.send(iosDirectoryList);
-});
-
-app.get('/karate', function(req, res){
-    var karateDirectoryList = []
-    console.log(req.url);
-    const karateFolder =  './allure-results/karate'
-    fs.readdirSync(karateFolder).forEach(file => {
-        if (fileNameRegex.test(file)) {
-            karateDirectoryList.push(file);
-                app.get('/'+file, function(req, res){ 
-                 res.sendFile( __dirname + '/allure-results/karate/'+file+'/index.html');    
-                });
-        }
-    });
-    console.log(karateDirectoryList);
-    res.send(karateDirectoryList);
 });
 
 
@@ -87,3 +31,19 @@ app.listen(80, function(){
     console.log('Server running at http://127.0.0.1:80/');
 });
 
+
+function getTosReportByFolderName(tosFile,req,res){
+    var directoryList = []
+    console.log(req.url);
+    const tosInnerFolder =  './allure-results/'+ tosFile
+    fs.readdirSync(tosInnerFolder).forEach(file => {
+        if (fileNameRegex.test(file)) {
+            directoryList.push(file);
+                app.get('/'+file, function(req, res){ 
+                 res.sendFile( __dirname + '/allure-results/bddweb/'+file+'/index.html');    
+                });
+        }
+    });
+    console.log(directoryList);
+    res.send(directoryList);
+}
