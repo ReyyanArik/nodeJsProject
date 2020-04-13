3c6bd398e67d91d067f491a5349d4ab7c6fdad62
Reyyan ARIK <reyyan.arik@nesine.com>
2020-04-13 13:00:52 +0300

    refactor

--- myserver.js
+++ myserver.js
@@ -6,14 +6,11 @@ var fs = require("fs")
 var app = express();
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/allure-results'));
-app.use(express.static(__dirname + '/allure-results/bddweb'));
-app.use(express.static(__dirname + '/allure-results/bddandroid'));
-app.use(express.static(__dirname + '/allure-results/bddios'));
-app.use(express.static(__dirname + '/allure-results/karate'));
 
 var home = __dirname + "/boxes.html";
-var tosProjects = ["bddweb","bddandroid","bddios","karate"];
+var tosProjects = ["bddweb","android","ios","karateapi"];
 var fileNameRegex = new RegExp(/\d{14}/);
+var allureFolder = '/allure-results/';
 
 app.get('/', function(req, res) {
     console.log(req.url);
@@ -21,26 +18,25 @@ app.get('/', function(req, res) {
 })
 
 tosProjects.forEach(tosProject =>{
+    app.use(express.static(__dirname + allureFolder + tosProject));
     app.get('/'+tosProject, function(req, res){
         getTosReportByFolderName(tosProject,req,res);
     });
 });
 
-
 app.listen(80, function(){
     console.log('Server running at http://127.0.0.1:80/');
 });
 
-
 function getTosReportByFolderName(tosFile,req,res){
     var directoryList = []
     console.log(req.url);
-    const tosInnerFolder =  './allure-results/'+ tosFile
+    const tosInnerFolder =  '.' + allureFolder + tosFile
     fs.readdirSync(tosInnerFolder).forEach(file => {
         if (fileNameRegex.test(file)) {
             directoryList.push(file);
                 app.get('/'+file, function(req, res){ 
-                 res.sendFile( __dirname + '/allure-results/'+tosfile+'/'+file+'/index.html');    
+                    res.sendFile( __dirname + allureFolder + tosfile +'/' + file + '/index.html');    
                 });
         }
     });
